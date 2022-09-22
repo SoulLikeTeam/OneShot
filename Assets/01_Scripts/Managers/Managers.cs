62c93956102f06df37acb1994f98b531fb1e477d
ehddbs <ehddbs295@gmail.com>
2022-09-22 19:09:37 +0900

    Manager Setting

--- Assets/01_Scripts/Managers/Managers.cs
+++ Assets/01_Scripts/Managers/Managers.cs
@@ -10,9 +10,13 @@ public class Managers : MonoBehaviour
 
     #region CORE
     //Manager
-
+    PoolManager _pool = new PoolManager();
+    SceneManagerEX _scene = new SceneManagerEX();
+    ResourceManagers _resource = new ResourceManagers();
     //Property
-
+    public static PoolManager Pool { get { return Instance._pool; } }
+    public static SceneManagerEX Scene { get { return Instance._scene; } }
+    public static ResourceManagers Resource { get { return Instance._resource; } }
     #endregion
 
     private void Start()
@@ -21,11 +25,25 @@ public class Managers : MonoBehaviour
     }
     static void Init()
     {
+        if (instance == null)
+        {
+            GameObject go = GameObject.Find("@Managers");
+            if (go == null)
+            {
+                go = new GameObject { name = "@Managers" };
+                go.AddComponent<Managers>();
+            }
+            DontDestroyOnLoad(go);
+            instance = go.GetComponent<Managers>();
 
+            instance._pool.Init();
+        }
     }
-
     static void Clear()
     {
+        Scene.Clear();
 
+
+        Pool.Clear(); //항상 마지막에 클리어
     }
 }
--- Assets/01_Scripts/Managers/ResourceManagers.cs
+++ Assets/01_Scripts/Managers/ResourceManagers.cs
@@ -4,5 +4,45 @@ using UnityEngine;
 
 public class ResourceManagers
 {
-    
+    public T Load<T>(string path) where T : Object
+    {
+        if(typeof(T) == typeof(GameObject))
+        {
+            string name = path;
+            int index = name.LastIndexOf('/');
+            if (index >= 0)
+                name = name.Substring(index + 1);
+
+            //GameObject go = Managers
+        }
+        return Resources.Load<T>(path);
+    }
+
+    public GameObject Instantiate(string path,Transform parent = null)
+    {
+        GameObject original = Load<GameObject>($"Prefabs/{path}");
+        if(original==null)
+        {
+            Debug.Log($"Failed to load prefab : {path}");
+            return null;
+        }
+
+        GameObject go = Object.Instantiate(original, parent);
+        go.name = original.name;
+        return go;
+    }
+    public void Destroy(GameObject go)
+    {
+        if (go == null)
+            return;
+
+        Poolable poolable = go.GetComponent<Poolable>();
+        if (poolable != null)
+        {
+            Managers.Pool.Push(poolable);
+            return;
+        }
+
+        Object.Destroy(go);
+    }
 }
