a1fc4a43f01e30e5f46147ba4501927bda0869a8
kjp2981 <81199996+kjp2981@users.noreply.github.com>
2022-10-05 20:24:13 +0900

    fsm 몇 기능 추가 및 최적화?

---
+++ Assets/Kjp/Scripts/FSM/AIBrain.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIBrain : MonoBehaviour
+{
+    //private List<AIState> _stateList;
+
+    [SerializeField]
+    private GameObject _target;
+
+    private AIState _beforeState;
+    [SerializeField]
+    private AIState _currentState;
+
+    public AIState GetCurrentState()
+    {
+        return _currentState;
+    }
+
+    public void ChangeState(AIState state)
+    {
+        _beforeState = _currentState;
+        _currentState = state;
+        _beforeState.OnStateLeave();
+        _currentState.OnStateEnter();
+    }
+
+    //private void Awake()
+    //{
+    //    transform.GetComponentsInChildren<AIState>(_stateList);
+    //}
+
+    private void Update()
+    {
+        if (_target != null)
+        {
+            _currentState.TakeAAction();
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        ConditionPair nextCondition = null;
+        foreach (ConditionPair pair in _currentState._transitionList)
+        {
+            bool isTransition = true;
+            for (int i = 0; i < pair.conditionList.Count; i++)
+            {
+                if (pair.conditionList[i].IfCondition(_currentState, pair.nextState))
+                {
+                    isTransition = true;
+                }
+                else isTransition = false;
+            }
+
+            if (isTransition == true)
+            {
+                if (nextCondition == null)
+                {
+                    nextCondition = pair;
+                }
+                else
+                {
+                    if (pair.priority > nextCondition.priority)
+                    {
+                        nextCondition = pair;
+                    }
+                }
+            }
+        }
+
+        if (nextCondition != null)
+        {
+            ChangeState(nextCondition.nextState);
+        }
+    }
+}
